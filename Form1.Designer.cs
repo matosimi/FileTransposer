2d0879594a11dd0bfe0024d5777a0372c9f94282
Martin Simecek <martin.simecek@dxc.com>
2023-03-16 10:05:03 +0100

    first commit v0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace FileTransposer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            buttonOpen = new Button();
+            textBoxOriginal = new TextBox();
+            textBoxTransposed = new RichTextBox();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            buttonTranspose = new Button();
+            numericUpDownTranspose = new NumericUpDown();
+            buttonCut = new Button();
+            numericUpDownCut = new NumericUpDown();
+            textBoxLog = new TextBox();
+            textBoxChars = new TextBox();
+            buttonSave = new Button();
+            toolTip1 = new ToolTip(components);
+            radioButtonMads = new RadioButton();
+            radioButtonMemDump = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownTranspose).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownCut).BeginInit();
+            SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            buttonOpen.Location = new Point(12, 12);
+            buttonOpen.Name = "buttonOpen";
+            buttonOpen.Size = new Size(75, 23);
+            buttonOpen.TabIndex = 0;
+            buttonOpen.Text = "Open";
+            buttonOpen.UseVisualStyleBackColor = true;
+            buttonOpen.Click += ButtonOpen_Click;
+            // 
+            // textBoxOriginal
+            // 
+            textBoxOriginal.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBoxOriginal.Location = new Point(12, 215);
+            textBoxOriginal.Multiline = true;
+            textBoxOriginal.Name = "textBoxOriginal";
+            textBoxOriginal.ScrollBars = ScrollBars.Both;
+            textBoxOriginal.Size = new Size(279, 356);
+            textBoxOriginal.TabIndex = 1;
+            textBoxOriginal.WordWrap = false;
+            // 
+            // textBoxTransposed
+            // 
+            textBoxTransposed.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBoxTransposed.Location = new Point(296, 15);
+            textBoxTransposed.Multiline = true;
+            textBoxTransposed.Name = "textBoxTransposed";
+            textBoxTransposed.ScrollBars = RichTextBoxScrollBars.Both;
+            textBoxTransposed.Size = new Size(791, 194);
+            textBoxTransposed.TabIndex = 2;
+            textBoxTransposed.WordWrap = false;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonTranspose
+            // 
+            buttonTranspose.Location = new Point(12, 110);
+            buttonTranspose.Name = "buttonTranspose";
+            buttonTranspose.Size = new Size(75, 23);
+            buttonTranspose.TabIndex = 3;
+            buttonTranspose.Text = "Transpose";
+            buttonTranspose.UseVisualStyleBackColor = true;
+            buttonTranspose.Click += ButtonTranspose_Click;
+            // 
+            // numericUpDownTranspose
+            // 
+            numericUpDownTranspose.Location = new Point(120, 110);
+            numericUpDownTranspose.Name = "numericUpDownTranspose";
+            numericUpDownTranspose.Size = new Size(68, 23);
+            numericUpDownTranspose.TabIndex = 4;
+            toolTip1.SetToolTip(numericUpDownTranspose, "number of groups");
+            numericUpDownTranspose.Value = new decimal(new int[] { 9, 0, 0, 0 });
+            // 
+            // buttonCut
+            // 
+            buttonCut.Location = new Point(12, 81);
+            buttonCut.Name = "buttonCut";
+            buttonCut.Size = new Size(75, 23);
+            buttonCut.TabIndex = 5;
+            buttonCut.Text = "Cut";
+            buttonCut.UseVisualStyleBackColor = true;
+            buttonCut.Click += ButtonCut_Click;
+            // 
+            // numericUpDownCut
+            // 
+            numericUpDownCut.Location = new Point(120, 81);
+            numericUpDownCut.Name = "numericUpDownCut";
+            numericUpDownCut.Size = new Size(68, 23);
+            numericUpDownCut.TabIndex = 6;
+            toolTip1.SetToolTip(numericUpDownCut, "bytes to be cut from the beginning of the file");
+            numericUpDownCut.Value = new decimal(new int[] { 81, 0, 0, 0 });
+            // 
+            // textBoxLog
+            // 
+            textBoxLog.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBoxLog.Location = new Point(11, 139);
+            textBoxLog.Multiline = true;
+            textBoxLog.Name = "textBoxLog";
+            textBoxLog.ScrollBars = ScrollBars.Both;
+            textBoxLog.Size = new Size(279, 70);
+            textBoxLog.TabIndex = 7;
+            // 
+            // textBoxChars
+            // 
+            textBoxChars.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBoxChars.Location = new Point(297, 215);
+            textBoxChars.Multiline = true;
+            textBoxChars.Name = "textBoxChars";
+            textBoxChars.ScrollBars = ScrollBars.Both;
+            textBoxChars.Size = new Size(466, 203);
+            textBoxChars.TabIndex = 8;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(113, 12);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(75, 23);
+            buttonSave.TabIndex = 9;
+            buttonSave.Text = "Save";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += ButtonSave_Click;
+            // 
+            // radioButtonMads
+            // 
+            radioButtonMads.AutoSize = true;
+            radioButtonMads.Location = new Point(875, 224);
+            radioButtonMads.Name = "radioButtonMads";
+            radioButtonMads.Size = new Size(99, 19);
+            radioButtonMads.TabIndex = 10;
+            radioButtonMads.Text = "Output MADS";
+            radioButtonMads.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMemDump
+            // 
+            radioButtonMemDump.AutoSize = true;
+            radioButtonMemDump.Checked = true;
+            radioButtonMemDump.Location = new Point(875, 249);
+            radioButtonMemDump.Name = "radioButtonMemDump";
+            radioButtonMemDump.Size = new Size(129, 19);
+            radioButtonMemDump.TabIndex = 11;
+            radioButtonMemDump.TabStop = true;
+            radioButtonMemDump.Text = "Output mem dump";
+            radioButtonMemDump.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1143, 607);
+            Controls.Add(radioButtonMemDump);
+            Controls.Add(radioButtonMads);
+            Controls.Add(buttonSave);
+            Controls.Add(textBoxChars);
+            Controls.Add(textBoxLog);
+            Controls.Add(numericUpDownCut);
+            Controls.Add(buttonCut);
+            Controls.Add(numericUpDownTranspose);
+            Controls.Add(buttonTranspose);
+            Controls.Add(textBoxTransposed);
+            Controls.Add(textBoxOriginal);
+            Controls.Add(buttonOpen);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDownTranspose).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownCut).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonOpen;
+        private TextBox textBoxOriginal;
+        private RichTextBox textBoxTransposed;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private Button buttonTranspose;
+        private NumericUpDown numericUpDownTranspose;
+        private Button buttonCut;
+        private NumericUpDown numericUpDownCut;
+        private TextBox textBoxLog;
+        private TextBox textBoxChars;
+        private Button buttonSave;
+        private ToolTip toolTip1;
+        private RadioButton radioButtonMads;
+        private RadioButton radioButtonMemDump;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+using System.Globalization;
+using System.Text;
+
+namespace FileTransposer
+{
+    public partial class Form1 : Form
+    {
+        private static byte[] data = { };
+        private static byte[] transposedData;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "noFile";
+            UpdateTitle();
+        }
+
+        private void ButtonOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                data = File.ReadAllBytes(openFileDialog1.FileName);
+                PrintOriginal();
+                UpdateTitle();
+            }
+        }
+
+        private void PrintOriginal()
+        {
+            int count = 0;
+            string hexdata = Convert.ToHexString(data);
+            string output = "";
+            textBoxChars.Text = Encoding.ASCII.GetString(data);
+            for (int line = 0; line < hexdata.Length / 16; line++)
+            {
+                /* byte[] bytes = new byte[16];
+                 try
+                 {
+                     Array.Copy(data, line * 16, bytes, 0, 16);
+                 }
+                 catch (Exception ex)
+                 {
+                     textBoxLog.Text = ex.Message;
+                 }
+                */
+                string hexspace = hexdata.Substring(line * 16, 16);
+                // string chars = Encoding.ASCII.GetString(bytes);
+                for (int i = 2; i < hexspace.Length; i += 2)
+                    hexspace = hexspace.Insert(i++, " ");
+
+                output += $"{(line * 16):X4}: {hexspace}" + Environment.NewLine;
+            }
+            textBoxOriginal.Text = output;
+        }
+
+        private void UpdateTitle()
+        {
+            this.Text = $"FileTransposer 0.1 by MatoSimi 16.3.2023 - {openFileDialog1.FileName} - {data.Length}(${data.Length:X4}) bytes";
+        }
+
+        private void ButtonCut_Click(object sender, EventArgs e)
+        {
+            byte[] newdata = new byte[data.Length - (int)numericUpDownCut.Value];
+            Array.Copy(data, (int)numericUpDownCut.Value, newdata, 0, newdata.Length);
+            data = newdata;
+            PrintOriginal();
+            UpdateTitle();
+        }
+
+        private void ButtonTranspose_Click(object sender, EventArgs e)
+        {
+            textBoxTransposed.Text = "";
+            int frameSize = (int)numericUpDownTranspose.Value;
+            int frameCount = data.Length / frameSize;
+            string[] output = new string[frameSize];
+            int index = 0;
+            int overdrive = 0;
+            transposedData = new byte[data.Length];
+            while (index < data.Length)
+            {
+                for (int line = 0; line < frameSize; line++)
+                    if (index + line < data.Length)
+                    {
+                        output[line] += $"${data[index + line]:X2} ";
+                    }
+                    else
+                    {
+                        output[line] += "XX ";
+                        overdrive++;
+                    }
+                index += frameSize;
+            }
+
+            for (int i = 0; i < data.Length; i++)
+                transposedData[(i % frameSize) * frameCount + i / frameSize] = data[i];
+
+            textBoxLog.Text = $"{(int)(data.Length / frameSize)} frames, {overdrive} bytes orphaned";
+            int length = 0;
+            for (int line = 0; line < frameSize; line++)
+            {
+                if (radioButtonMemDump.Checked)
+                {
+                    textBoxTransposed.Text += $"{length:X4}: {output[line]}{Environment.NewLine}";
+                }
+                else
+                {
+                    for (int j = 0; j < output[line].Length/16/3; j++)
+                    {
+                        if (j == 0)
+                            textBoxTransposed.Text += $"group{line}\tdta {output[line].Substring(0,16*3-1).Replace(' ',',')}{Environment.NewLine}";
+                        else
+                            textBoxTransposed.Text += $"\tdta {output[line].Substring(j*16*3, 16 * 3-1).Replace(' ', ',')}" + (j == 0 ? $"\t;{length:X4}" : "") + $"{Environment.NewLine}";
+                    }
+                }
+                length += (int)(data.Length / frameSize);
+            }
+
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(saveFileDialog1.FileName, transposedData);
+                textBoxLog.Text = $"File {saveFileDialog1.FileName} saved.";
+            }
+        }
+    }
+}
+
